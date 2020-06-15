153fd8c4c59bf5d2f42ac61d5b7baa8ed289b144
AliAlduaij <aliaalduaij@gmail.com>
2020-06-15 15:00:02 +0300

    ,

--- story.js
+++ story.js
@@ -1,7 +1,8 @@
 // Define the name here on line 2
-
+name = "ali";
 // Define the age here on line 4
-
+age = 27;
 // Define the hobby here on line 6
-
+hobby = "martial arts";
 // Print the story here on line 8
+story = "Mr./Mrs." + name + ", in 33 years, you will be 60 years old";
